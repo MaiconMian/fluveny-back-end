b4cc8f9f58fc2965aa4873f1cd26693595ce8a1c
MaiconMian <maicon.mian@sou.unifal-mg.edu.br>
2025-05-12 18:45:06 -0300

    Fixing Dockerization and Test Environments

--- run.ps1
+++
@@ -1,6 +0,0 @@
-Remove-Item *.jar -Force
-mvn clean install -DskipTests
-Copy-Item target\*.jar -Destination fluveny.jar
-
-docker-compose build
-docker-compose up
--- run.sh
+++
@@ -1,5 +0,0 @@
-rm -f *.jar
-mvn clean install -DskipTests
-cp target/*.jar fluveny.jar
-
-docker-compose build && docker-compose up
